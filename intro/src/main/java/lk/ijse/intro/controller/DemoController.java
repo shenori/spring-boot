e8fe69b48e6edf9444879ba080d62e8a3e730d50
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-01-04 14:50:10 +0530

    Customer file fixed

--- intro/src/main/java/lk/ijse/intro/controller/DemoController.java
+++ intro/src/main/java/lk/ijse/intro/controller/DemoController.java
@@ -3,9 +3,12 @@ package lk.ijse.intro.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lk.ijse.intro.dto.Customer;
 
 @RestController
 @RequestMapping("/demo")
@@ -25,4 +28,20 @@ public class DemoController {
 		System.out.println("Query param1: " + param1);
         System.out.println("Query param2: " + param2);
 	}
+
+    @PostMapping("/body")
+    public void getRequestBodyData(@RequestBody String reqBodyData) {
+        System.out.println("Request body: " + reqBodyData);
+    }
+
+    @PostMapping("/customer")
+    public Customer saveCustomer(@RequestBody Customer customer) {
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getCity());
+
+        return customer; // must return something
+    }
+
+
 }
