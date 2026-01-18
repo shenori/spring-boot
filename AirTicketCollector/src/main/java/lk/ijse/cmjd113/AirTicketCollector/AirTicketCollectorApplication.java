9b117d184f46aed4eaad3580b1368578781aa25b
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-01-18 09:55:52 +0530

    Initial commit - Spring Boot project

---
+++ AirTicketCollector/src/main/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd113.AirTicketCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTicketCollectorApplication.class, args);
+	}
+
+}
---
+++ AirTicketCollector/src/test/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd113.AirTicketCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
