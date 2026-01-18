da1136c3ea4a765987f984c3687b80a9c391a776
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-01-18 10:47:06 +0530

    Initially Test - save an airport

--- AirTicketCollector/src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
+++ AirTicketCollector/src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
@@ -1,6 +1,10 @@
 package lk.ijse.cmjd113.AirTicketCollector.controller;
 
-
+import lk.ijse.cmjd113.AirTicketCollector.dto.AirportDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/airports")
 public class AirportController {
 
-}
+    // Save an airport
+    @PostMapping
+    public ResponseEntity<AirportDTO> saveAirport(
+            @RequestBody AirportDTO airportDTO) {
 
+        return new ResponseEntity<>(airportDTO, HttpStatus.CREATED);
+    }
+}
