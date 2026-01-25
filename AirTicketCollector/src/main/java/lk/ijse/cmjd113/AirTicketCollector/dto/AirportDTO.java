5a589a59659fbde3284b89a4e58925c16e3e539d
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-01-25 10:17:37 +0530

    Error Fixed

--- AirTicketCollector/src/main/java/lk/ijse/cmjd113/AirTicketCollector/dto/AirportDTO.java
+++ AirTicketCollector/src/main/java/lk/ijse/cmjd113/AirTicketCollector/dto/AirportDTO.java
@@ -1,20 +1,17 @@
 package lk.ijse.cmjd113.AirTicketCollector.dto;
 
-import java.io.Serializable;
-
+import java.io.Serializable; // MUST be at the top
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
-public class AirportDTO {
+public class AirportDTO implements Serializable {
     private String airportId;
     private String airportCode;
     private String name;
     private String city;
     private String country;
-
-
 }
